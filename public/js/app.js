99f41956beb6471af15f82e7bd1df542f8f70545
Ben Hulan <benhulan@gmail.com>
2015-10-14 17:11:52 -0700

    initial commit of live_code branch

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,53 @@
 // CLIENT-SIDE JAVASCRIPT
 // On page load
 $(document).ready(function(){
-  
+  pageLoad();
 });
+
+// function definitions
+
+function pageLoad() {
+  // set event listeners
+  $("#new-food-form").on("submit", function(e){
+    // prevent form submission
+    e.preventDefault();
+    // post serialized form to server
+    $.post("/api/foods", $(this).serialize(), function(response){
+      // append new food to the page
+      var newFood = response;
+      // clear new food form
+      var foodString = makeHTMLString(newFood);
+      $("#food-ul").prepend(foodString);
+      // reset the form 
+      $("#new-food-form")[0].reset();
+      // give focus back to the food name input
+      $("#food-name-input").focus();
+    });
+  });
+
+  // set event listener for all delete buttons
+  $(document).on('click', 'button.close', function(e){
+    deleteFood(this);
+  });
+}
+
+function deleteFood(context) {
+  console.log('context in deleteFood: ', context);
+  // context is the button that was clicked
+  var foodId = $(context).data().id;
+  $.ajax({
+    url: '/api/foods/' + foodId,
+    type: 'DELETE',
+    success: function(response) {
+      // once successful, remove food from the DOM
+      $(context).closest('li').remove();
+    }
+  });
+}
+
+function makeHTMLString(food) {
+  return "<li class='list-group-item'>" + food.name + 
+    " <span class='label label-default'>"+food.yumminess+"</span>" +
+    "<button data-id="+food.id+" type='button' class='close' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+    "</li>";
+}
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express = require("express"),
     path = require("path"),
     bodyParser = require("body-parser");
 
+var where = require("./utils/where");
+
 // CONFIG //
 // set ejs as view engine
 app.set('view engine', 'ejs');
@@ -22,6 +24,47 @@ var foods =[
   {id: 3, name: "Foie Gras", yumminess: "omg"},
   {id: 4, name: "Kale", yumminess: "meh"}
 ];
+
+// ROUTES //
+app.get("/", function (req, res){
+  // render index.html and send with foods data filled in
+  res.render('index', {foods: foods});
+});
+
+// api route to get all foods (we don't use)
+app.get("/api/foods", function (req, res){
+  // send foods data as JSON
+  res.json(foods);
+});
+
+// api route to create new food
+app.post("/api/foods", function (req, res){
+  var newFood = req.body;
+  // add a unique id
+  if (foods.length !== 0){
+	  newFood.id = foods[foods.length - 1].id + 1;
+  } else {
+  	newFood.id = 0;
+  }
+  // add new food to DB (which, in this case, is an array)
+  foods.push(newFood);
+  // send a response with newly created object
+  res.json(newFood);
+});
+
+// api route to delete a food
+app.delete("/api/foods/:id", function (req, res){
+  // set the value of the id
+  var targetId = parseInt(req.params.id);
+  // find item in the array matching the id
+  var targetItem = where(foods, {id: targetId});
+  // get the index of the found item
+  var index = foods.indexOf(targetItem);
+  // remove the item at that index, only remove 1 item
+  foods.splice(index, 1);
+  // send back deleted object
+  res.json(targetItem);
+});
 
 app.listen(3000, function (){
   console.log("listening on port 3000");
